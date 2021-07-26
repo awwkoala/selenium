dd3bfbf617a006b0e34d3e267f7d008857196e4b
kmatylewska <karolinam.m@wp.pl>
2021-07-26 16:20:08 +0200

    Review fixes

--- src/main/java/com/github/awwkoala/selenium/page/FileUploadPage.java
+++ src/main/java/com/github/awwkoala/selenium/page/FileUploadPage.java
@@ -1,7 +1,6 @@
 package com.github.awwkoala.selenium.page;
 
 import com.github.awwkoala.selenium.util.PathUtils;
-import com.github.awwkoala.selenium.util.WaitUtils;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -30,8 +29,7 @@ public class FileUploadPage extends BasePage {
   }
 
   public FileUploadPage chooseFileToUpload(String filename) {
-    PathUtils pathUtils = new PathUtils();
-    String absolutePath = pathUtils.getPathFromResources(Paths.get(filename)).toAbsolutePath().toString();
+    String absolutePath = PathUtils.getPathFromResources(Paths.get(filename)).toAbsolutePath().toString();
     waitUtils.waitUntilClickable(chooseFileBtn);
     chooseFileBtn.sendKeys(absolutePath);
     return this;
--- src/main/java/com/github/awwkoala/selenium/util/PathUtils.java
+++ src/main/java/com/github/awwkoala/selenium/util/PathUtils.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public class PathUtils {
 
-  public Path getPathFromResources(Path filename) {
+  public static Path getPathFromResources(Path filename) {
     return Paths.get("src", "main", "resources").resolve(filename);
   }
 
